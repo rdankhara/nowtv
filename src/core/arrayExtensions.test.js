da6d971f1eb166bd4c01daec4a1971878ef727d8
rdankhara <rajnikant.dankhara@gmail.com>
2018-08-29 17:54:51 +0100

    added test for argument type error

--- src/core/arrayExtensions.test.js
+++ src/core/arrayExtensions.test.js
@@ -10,4 +10,13 @@ describe('toMap', ()=>{
         expect(first.id).toBe(1);
         expect(first.name).toEqual("name1");
     });
+
+    it('throws error when argument passed is not an array', ()=> {
+        const nonArray = {id: 1, name : "name1"};
+        expect(() => toMap(nonArray, "name")).toThrow();
+    });
+
+    it ('throws error with message argument arr is not an array when argument passed is not an array', () => {
+        expect(() => toMap({}, "name")).toThrowError('argument arr is not an array');
+    });
 })
