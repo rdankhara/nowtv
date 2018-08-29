640ce1da405514ebc66f18f178f3823d00bbb676
rdankhara <rajnikant.dankhara@gmail.com>
2018-08-29 17:28:56 +0100

    Added clip path

--- src/containers/components/Message.js
+++ src/containers/components/Message.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import './Message.css';
+import * as classes from './Message.css';
 const Message = (props) => {
     return(
         <li>
-            <article className="flex-container">
-                <div className="avatar-container">
-                    <a className="tooltip">
-                        <img src={props.avatar} alt="" className="avatar tip" />
-                        <span className="tooltiptext">{props.email}</span>
-                    </a>
-                </div>
+            <article className="flexContainer">
+                <a className="tooltip">
+                    <img src={props.avatar} alt="" className="avatar tip" />
+                    <span className="tooltiptext">{props.email}</span>
+                </a>
                 <div className="message-container">
                     <h2 className="fullName">{props.fullName}</h2>
-                    <h5 className="timestamp">{ props.timestamp ? props.timestamp.toDateString() : ''}</h5>
+                    { props.timestamp && 
+                        <h5 className="timestamp">props.timestamp.toDateString()</h5>
+                    }
                     <p className="message">{props.message}</p>
                 </div>
             </article>
