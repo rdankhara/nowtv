ce36730e61cb7bfbbf68327e48aa9f71d1496d8f
rdankhara <rajnikant.dankhara@gmail.com>
2018-11-10 15:52:32 +0000

    higher order function refactored using lambda

--- src/reducers/messageActions.js
+++ src/reducers/messageActions.js
@@ -1,7 +1,8 @@
 import * as actions from './actions';
 import {getChatMessages} from '../service';
 
-export const getMessagesAsync =  () => {
+//classic way function returning function
+export const getMessagesAsyncClassic =  () => {
     
     return async dispatch => {
 
@@ -14,4 +15,15 @@ export const getMessagesAsync =  () => {
             dispatch(actions.getMessageFailed(error.message));
         }
     };
+};
+
+//chaining function return function using arrow function
+export const getMessagesAsync =  () => async dispatch => {
+    dispatch(actions.getMessageAction());
+    try {
+        const data = await getChatMessages();
+        dispatch(actions.getMessageSuccess(data));
+    } catch (error) {
+        dispatch(actions.getMessageFailed(error.message));
+    }
 };
